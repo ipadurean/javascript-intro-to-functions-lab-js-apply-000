f926973afaba188f336ec53f94adbb1d7e6da017
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-04-29 19:00:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
 }
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too";
+  }
+}
+
