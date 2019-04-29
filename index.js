c30c011cd628e886bfd126d068dd8dfd1fe57d6c
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-04-29 19:15:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    console.log("'I can't hear you!'");
+    console.log('I can\'t hear you!');
   }
 }
 
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
-    return "YES INDEED!";
+    return 'YES INDEED!';
   }
 }
 
